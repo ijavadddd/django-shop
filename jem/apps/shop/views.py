e26fcf4909fb2aa8ced53a0e4770a6c49b526d06
ijavadddd <ijavaddddd@gmail.com>
2022-04-15 22:35:15 +0330

    Add Media_URl to project context

--- jem/apps/shop/views.py
+++ jem/apps/shop/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from django.conf import settings
+
 # Create your views here.
 
 
 ###This class will render home page
+def Media(request):
+    context={
+        'media':settings.MEDIA_URL,
+    }
+    return context
+
+
 class Index(View):
     def get(self,request):
         return render(request, 'shop/index.html')
